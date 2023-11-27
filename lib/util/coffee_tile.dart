1a9df0fa73e159ae7c7556ef7d5a7b3c377a6509
Ojoxux <ojoxux@gmail.com>
2023-11-27 12:55:02 +0900

    First Commit!

---
+++ lib/util/coffee_tile.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class CoffeeTile extends StatelessWidget {
+  const CoffeeTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        width: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.black54,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //coffee image
+            ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset('lib/images/latte.jpg')),
+
+            //coffee name
+            Padding(
+              padding: 
+                const EdgeInsets.symmetric(vertical: 12.0,horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Latte',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    'With almond Milk',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                ],
+              ),
+            ),
+
+            // coffee price
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('￥400'),
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Icon(Icons.add)
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
